eb5ebc99819cce7aadde6232466ff2158453cbcd
otknoy <otknoy@gmail.com>
2016-02-19 23:54:40 +0900

    Script for experiment

--- experiment/ec_exp.js
+++ experiment/ec_exp.js
@@ -1,30 +1,51 @@
 var Condition = require('./condition.js');
 
+// subjects
+var fs = require('fs');
+var filename = process.argv[2];
+var subjects = JSON.parse(fs.readFileSync(filename, 'utf8'));
+
 // test data
-var samples = require('../src/test/data.json');
+var targets = require('../src/test/data.json');
+
+
+// experiment
+var result = [];
+for (var s_i = 0; s_i < subjects.length; s_i++) {
+    var subject = subjects[s_i];
+    var ts1 = subject.points;
+
+    for (var t_i = 0; t_i < targets.length; t_i++) {
+	var target = targets[t_i];
+	var ts2 = target.points;
+
+	// similarity measure methods
+	var dtw = Condition.dtw;	// baseline
+	var dtw_ts = Condition.dtw_ts;  // approach
+
+	var baselineScore = dtw(ts1, ts2);
+	var approachScore = dtw_ts(ts1, ts2);
+
+
+	result.push({
+	    subject: subject.name,
+	    target:  target.name,
+	    baseline: baselineScore,
+	    approach: approachScore
+	});
+    }
+}
+
+
+// output result
+console.log('subject,target,baseline,approach');
+result.forEach(function(e, i, ary) {
+    // output result
+    console.log(e.subject + ',' + e.target + ',' + e.baseline + ',' + e.approach);
+});
+
 
 // for sorting
 function resultCompare(a, b) {
     return a.score - b.score;
-}
-
-// subject's query
-var query = samples[0];
-
-var ts1 = query.points;
-
-var result = [];
-
-console.log("ts1_name,ts2_name,score");
-
-for (var i = 0; i < samples.length; i++) {
-    var ts2 = samples[i].points;
-
-    // var distance = Condition.baseline;
-    // var distance = Condition.dtw;
-    var distance = Condition.dtw_ts;
-
-    var score = distance(ts1, ts2);
-
-    console.log(query.name + "," + samples[i].name + "," + score);
 }
