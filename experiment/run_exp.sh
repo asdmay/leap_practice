5db7582c81caf33bc6effdcbec88213899ce99e2
otknoy <otknoy@gmail.com>
2016-02-21 14:10:23 +0900

    Update run_exp.sh

--- experiment/run_exp.sh
+++ experiment/run_exp.sh
@@ -3,6 +3,7 @@
 subjects_dir=../../ashidaResearch/experiment/subjects
 output_dir=./result
 
+# exp
 for i in `seq -w 20`;
 do
     subject=${subjects_dir}/experiment${i}.json
@@ -11,4 +12,11 @@ do
     echo "${subject} -> experiment -> ${result}"
 
     node ec_exp.js ${subject} > ${result}
+done
+
+
+# rank
+for i in `seq -w 20`;
+do
+    ./test/rank.py ./result/experiment${i}_result.csv;
 done
